d3c9e64443101b5f2c9cee29775f40546fed6d49
Saurabh singh <ajinkyabisht75@gmail.com>
2026-02-25 11:20:37 +0530

    Updates

--- assets/mail-adKrQrHH.js
+++ assets/mail-adKrQrHH.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-h3I9q4ft.js";/**
+import{c as e}from"./index-CFFQlYsn.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
