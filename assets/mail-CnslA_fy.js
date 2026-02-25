a5f54c29de325b3f43e6bea5dd4cc56d7192f4d7
Saurabh singh <ajinkyabisht75@gmail.com>
2026-02-25 11:11:50 +0530

    Updates

--- assets/mail-CnslA_fy.js
+++ assets/mail-CnslA_fy.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-Cgx2U23W.js";/**
+import{c as e}from"./index-h3I9q4ft.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
