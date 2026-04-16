fdd77d0c2ad9be67149587c8b374b3859ade2011
Saurabh singh <ajinkyabisht75@gmail.com>
2026-04-16 21:04:39 +0530

    Updates

--- assets/mail-CCcFXyyF.js
+++ assets/mail-CCcFXyyF.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CFFQlYsn.js";/**
+import{c as e}from"./index-E3GW6Bcl.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
